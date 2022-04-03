5bb8c99bac5d2443ffb5228aac8f7525088f0626
Haider Ali <haiderali@192.168.10.10>
2022-04-03 14:41:08 -0700

    cors added

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,14 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDb from "./config/db.js";
 import userRouter from "./routes/userRoutes.js";
+import cors from "cors";
 dotenv.config();
 const PORT = process.env.PORT;
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 const app = express();
 app.use(express.json());
 
